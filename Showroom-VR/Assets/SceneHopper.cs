e96d815f09e8958db85b2725b422a69fa0156427
adnov <adam.novotnik@gmail.com>
2021-08-14 18:40:12 +0200

    vr scene
    teleportation, robot button, dimming light, moving columns, bloom

---
+++ Showroom-VR/Assets/SceneHopper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHopper : MonoBehaviour
+{
+    [SerializeField] public int sceneIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    public void HandleInput()
+    {
+       // if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //SceneManager.LoadSceneAsync("SwitchToKuka", LoadSceneMode.Additive);
+        //    SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Single);
+        }
+    }
+
+    public void Load2x2()
+    {
+        SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+    }
+
+    public void Load3x2()
+    {
+        SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+    }
+
+    public void Load3x3()
+    {
+        SceneManager.LoadSceneAsync(2, LoadSceneMode.Single);
+    }
+
+}
---
+++ Showroom-VR/Assets/Scripts/KukaController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KukaController : MonoBehaviour
+{
+    public Animator anim;
+   
+
+    public void AnimStart (string animName)
+    {
+        anim.SetTrigger(animName);
+    }
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            //print("one");
+            anim.SetTrigger("anim1");
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {  
+            anim.SetTrigger("anim2");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("anim3");
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetBool("anim4", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            anim.SetBool("anim4", false);
+        }
+
+        
+    
+        
+    }
+}
